a203f850e2f54a67921de7b51af1a18a54982df1
Tuncayarda <tncyard@gmail.com>
2025-05-05 02:07:21 +0300

    enviroment

---
+++ pico_src/app.cpp
@@ -0,0 +1,13 @@
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
+#include <rmw_microros/rmw_microros.h>
+#include <rover_interfaces/msg/servo_command.h>
+
+int main()
+{
+
+	return 0;
+}
